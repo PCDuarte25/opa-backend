8e305a49bd01ece972fd669b6a7b2147795470bd
Lucas Barreto <lucas_barreto29@outlook.com>
2023-09-25 14:18:00 -0300

    testando uma parada

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,24 +4,18 @@ import { AppService } from './app.service';
 import { DatabaseModule } from './datasources/database.module';
 import { ConfigModule } from '@nestjs/config';
 import { OpaPersonModule } from './opa_person/opa_person.module';
-<<<<<<< HEAD
 import { EncrypterModule } from './shared/services/encrypter/encrypter.module';
 import { AuthModule } from './auth/auth.module';
-=======
 import { StockModule } from './stock/stock.module';
->>>>>>> 7f2f43d (feat/corrigido endpoint de user person)
 
 @Module({
   imports: [
     DatabaseModule,
     ConfigModule.forRoot(),
     OpaPersonModule,
-<<<<<<< HEAD
     EncrypterModule,
     AuthModule,
-=======
     StockModule
->>>>>>> 7f2f43d (feat/corrigido endpoint de user person)
   ],
   controllers: [AppController],
   providers: [AppService],
