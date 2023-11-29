0b38b4d97ad1ed29eaeeb13ba5622c21ceb26857
Lucas Barreto <lucas_barreto29@outlook.com>
2023-11-28 21:47:19 -0300

    fix: fixed stock controller

--- src/stock/stock.controller.ts
+++ src/stock/stock.controller.ts
@@ -1,42 +1,60 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
-import { StockService } from './stock.service';
+import { Inject, Injectable } from '@nestjs/common';
 import { CreateStockDto } from './dto/create-stock.dto';
 import { UpdateStockDto } from './dto/update-stock.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Stock } from './entities/stock.entity';
 import { ValidationException } from '../utils/exceptions';
 import { ProdutoExistente } from '../messages/exceptions.messages';
 
-@Controller('stock')
-export class StockController {
-  constructor(private readonly stockService: StockService) { }
+@Injectable()
+export class StockService {
+  constructor(
+    @Inject("STOCK_REPOSITORY")
+    private stockRepository: Repository<Stock>,
+  ) { }
 
-  @Post()
-  async create(@Body() createStockDto: CreateStockDto) {
+  async create(createStockDto: CreateStockDto[]): Promise<Stock[] | null> {
+    var stockEntities: CreateStockDto[]
 
-    const stock = await this.stockService.findOneByCode(createStockDto.productCode)
-    if (stock) {
-      throw new ValidationException(ProdutoExistente)
+    for (var stockDto of createStockDto) {
+      const stockExists = await this.findOneByDescription(stockDto.productDescription)
+      if (stockExists) {
+        throw new ValidationException(ProdutoExistente)
+      }
+
+      const stock: Stock = {
+        measurementUnit: stockDto.measurementUnit,
+        productDescription: stockDto.productDescription,
+        stockQuantity: stockDto.stockQuantity
+      }
+
+      const stockEntity = this.stockRepository.create(stock)
+      stockEntities.push(stockEntity)
     }
 
-    return await this.stockService.create(createStockDto);
+    return await this.stockRepository.save(stockEntities);
   }
 
-  @Get()
-  async findAll(@Query('name') name: string) {
-    return await this.stockService.findAll(name);
+  async findOne(id: number): Promise<Stock | null> {
+    return await this.stockRepository.findOneBy({ id });
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: number) {
-    return this.stockService.findOne(id);
+  async findOneByDescription(productDescription: string): Promise<Stock | null> {
+    return await this.stockRepository.findOneBy({ productDescription });
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateStockDto: UpdateStockDto) {
-    return this.stockService.update(+id, updateStockDto);
+  async findAll(name: string): Promise<Stock[]> {
+    return await this.stockRepository.createQueryBuilder('Stock')
+      .where('LOWER(productDescription) LIKE LOWER(:productDescription)', { productDescription: `%${name}%` }) // Consulta "LIKE" insensível a maiúsculas/minúsculas
+      .getMany();
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.stockService.remove(+id);
+  update(id: number, updateStockDto: UpdateStockDto) {
+    return `This action updates a #${id} stock`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} stock`;
   }
 }
